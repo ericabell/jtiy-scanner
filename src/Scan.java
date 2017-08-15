63a8857b499c728799d9210f6e0657b211d9b48e
Eric Abell <ericabell@users.noreply.github.com>
2017-08-14 20:46:21 -0500

    Changed scan to add sequence of input numbers

--- src/Scan.java
+++ src/Scan.java
@@ -1,16 +1,32 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Scan {
-    public static void main(String args[]) {
+//    public static void main(String args[]) {
+//        Scanner s = null;
+//        try {
+//            s = new Scanner(System.in);
+//
+////            while (s.hasNext()) {
+////                System.out.println(s.next());
+////            }
+//            while (s.hasNextInt()) {
+//                System.out.println(s.nextInt());
+//            }
+//        } finally {
+//            if (s != null) {
+//                s.close();
+//            }
+//        }
+//    }
+    public static void main(String[] args) throws IOException {
         Scanner s = null;
         try {
             s = new Scanner(System.in);
-
-//            while (s.hasNext()) {
-//                System.out.println(s.next());
-//            }
+            int total = 0;
             while (s.hasNextInt()) {
-                System.out.println(s.nextInt());
+                total += s.nextInt();
+                System.out.println("total=" + total);
             }
         } finally {
             if (s != null) {
