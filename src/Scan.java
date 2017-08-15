ceccdd658993bdcc6e4470884709fedfc90c42bc
Eric Abell <ericabell@users.noreply.github.com>
2017-08-14 20:42:59 -0500

    Changed to scan for ints

--- src/Scan.java
+++ src/Scan.java
@@ -6,8 +6,11 @@ public class Scan {
         try {
             s = new Scanner(System.in);
 
-            while (s.hasNext()) {
-                System.out.println(s.next());
+//            while (s.hasNext()) {
+//                System.out.println(s.next());
+//            }
+            while (s.hasNextInt()) {
+                System.out.println(s.nextInt());
             }
         } finally {
             if (s != null) {
